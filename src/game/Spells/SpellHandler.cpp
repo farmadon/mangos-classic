93d3a6c5c6b80c70988e49b30b46bbe052efe1ec
Flekz <davidparraausina@gmail.com>
2024-04-23 17:42:49 +0100

    merge module system
    (cherry picked from commit ba7aec6fc082ba757d5157655fc25e0ff226a062)

--- src/game/Spells/SpellHandler.cpp
+++ src/game/Spells/SpellHandler.cpp
@@ -30,6 +30,10 @@
 #include "Spells/SpellAuras.h"
 #include "Loot/LootMgr.h"
 
+#ifdef ENABLE_MODULES
+#include "ModuleMgr.h"
+#endif
+
 void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
 {
     uint8 bagIndex, slot;
@@ -142,6 +146,13 @@ void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
             Spell::SendCastResult(_player, spellInfo, SPELL_FAILED_BAD_TARGETS);
         return;
     }
+
+    _player->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_ITEM_USE);
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnUseItem(pUser, pItem))
+        return;
+#endif
 
     // Note: If script stop casting it must send appropriate data to client to prevent stuck item in gray state.
     if (!sScriptDevAIMgr.OnItemUse(pUser, pItem, targets))
