9bdf7c5a1fe5d7467078015191c9b0beff5212d8
Flekz <davidparraausina@gmail.com>
2025-01-23 16:54:25 +0000

    Add on get player class level info hook
    (cherry picked from commit b843c914f3a5a94cb1a0ddd11689cb951cba3242)

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -2758,6 +2758,10 @@ void Player::GiveLevel(uint32 level)
     PlayerClassLevelInfo classInfo;
     sObjectMgr.GetPlayerClassLevelInfo(plClass, level, &classInfo);
 
+#ifdef ENABLE_MODULES
+    sModuleMgr.OnGetPlayerClassLevelInfo(this, classInfo);
+#endif
+
     // send levelup info to client
     WorldPacket data(SMSG_LEVELUP_INFO, (4 + 4 + MAX_POWERS * 4 + MAX_STATS * 4));
     data << uint32(level);
@@ -2860,6 +2864,10 @@ void Player::InitStatsForLevel(bool reapplyMods)
     uint32 level = GetLevel();
     uint32 plClass = getClass();
     sObjectMgr.GetPlayerClassLevelInfo(plClass, level, &classInfo);
+
+#ifdef ENABLE_MODULES
+    sModuleMgr.OnGetPlayerClassLevelInfo(this, classInfo);
+#endif
 
     PlayerLevelInfo info;
     sObjectMgr.GetPlayerLevelInfo(getRace(), plClass, level, &info);
